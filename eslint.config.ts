201c1ebd4ef3a54acd1e3cfbf2d75f6b31a25497
oth682 <osmanounana@gmail.com>
2025-08-19 11:03:39 +0100

    [DONE] Feature:Setup projet Tauri avec Vite #186

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,16 +2,42 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import tailwind from 'eslint-plugin-tailwindcss'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    plugins: { js, react: pluginReact, tailwindcss: tailwind },
+    extends: [
+      'js/recommended',
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: { version: 'detect' },
+      tailwindcss: {
+        config: 'tailwind.config.js',
+        lintOnly: true, // désactive le warning de version Tailwind 4
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+    },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs', globals: globals.node },
+  },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      'src-tauri/target',
+    ],
+  },
 ])
